59ee7771fb6d5b55ee105356e98cfd46011e855e
GREGORY GOMEZ <A00512729@gmail.com>
2016-04-08 11:34:16 -0500

    Add name and role

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,14 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
+
+var name = "Gregory Onasis"; 
+
+var formattedName = HTMLheaderName.replace("%data%", name);
+
+var role = "ITI Engineer"; 
+
+var formattedRole = HTMLheaderRole.replace("%data%", role);
+
+$("#header").prepend(formattedRole); 
+$("#header").prepend(formattedName);  
